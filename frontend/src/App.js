da43c2935881ff7f9ac824487ccc9a8fa635d80d
dushyant264 <ddushyant89@gmail.com>
2024-01-29 23:09:37 +0530

    heading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import Homepage from "./Pages/Homepage";
 import { Route } from "react-router-dom";
 import Chatpage from "./Pages/Chatpage";
 
 function App() {
+  useEffect(() => {
+    // Change tab heading
+    document.title = 'Chat App';
+
+    // Change favicon
+    const favicon = document.querySelector('link[rel="icon"]');
+    if (favicon) {
+      favicon.href = './back.ico';
+    }
+  }, []);
   return (
     <div className="App">
       <Route path="/" component={Homepage} exact />
